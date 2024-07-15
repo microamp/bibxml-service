57095100ebd06f4c18cb9ec43f260a1ded47f3c5
Sangho Na <sangho@nsh.nz>
2024-07-15 15:50:42 +1200

    test: Add more tests for get_primary_docid

--- bib_models/tests/test_util.py
+++ bib_models/tests/test_util.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from unittest import TestCase
 
 from bib_models import DocID
@@ -22,6 +23,17 @@ class UtilTestcase(TestCase):
         self.assertIsInstance(primary_id, DocID)
         self.assertEqual(primary_id.id, primary_id_value)  # type: ignore
 
+    def test_get_primary_docid_dedup(self):
+        raw_ids: list[DocID] = [
+            DocID(id="id", type="type", primary=True),
+            DocID(id="id", type="type", primary=True),
+            DocID(id="id", type="type", primary=True),
+        ]
+        primary_id: Optional[DocID] = get_primary_docid(raw_ids)
+        self.assertIsNotNone(primary_id)
+        self.assertIsInstance(primary_id, DocID)
+        self.assertEqual(primary_id.id, "id")  # type: ignore
+
     def test_fail_get_primary_docid_if_no_primary_id(self):
         """
         get_primary_docid should return None if no entry has primary == True
@@ -33,3 +45,18 @@ class UtilTestcase(TestCase):
             DocID(id="id3", type="type3", scope="scope"),
         ]
         self.assertIsNone(get_primary_docid(raw_ids))
+
+    def test_fail_get_primary_docid_if_primary_id_with_scope(self):
+        raw_ids: list[DocID] = [
+            DocID(id="id", type="type", primary=True, scope="scope")
+        ]
+        primary_id: Optional[DocID] = get_primary_docid(raw_ids)
+        self.assertIsNone(primary_id)
+
+    def test_fail_get_primary_docid_if_no_raw_ids(self):
+        raw_ids: list[DocID] = []
+        primary_id: Optional[DocID] = get_primary_docid(raw_ids)
+        self.assertIsNone(primary_id)
+
+    def test_normalize_role(self):
+        pass
